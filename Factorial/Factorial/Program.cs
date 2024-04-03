edbbdc35f971f9b9c9a2d3b3b647c08de665fb44
gRenka <128580487+gRenka-with-40k-garlic@users.noreply.github.com>
2024-04-03 10:47:06 +0700

    ૮₍ ˶• ༝ •˶ ₎ა

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static long Factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+        else
+            return n * Factorial(n - 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите число:");
+        int num = int.Parse(Console.ReadLine());
+        long result = Factorial(num);
+        Console.WriteLine($"Факториал {num} равен " + result);
+    }
+}
